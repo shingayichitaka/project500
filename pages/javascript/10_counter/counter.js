739322f52225fa5dd3a67c76dc3d2124b2e0e038
shingayichitaka <shingayidambudzochitaka@gmail.com>
2022-08-19 01:07:27 +0200

    feat(pages/javascript): add counter project

---
+++ pages/javascript/10_counter/counter.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const display = document.querySelector(".display");
+const add = document.querySelector(".add");
+const lower = document.querySelector(".lower");
+
+add.addEventListener("click", function () {
+  console.log("Click");
+});
+lower.addEventListener("click", function () {
+  console.log("Click");
+});
