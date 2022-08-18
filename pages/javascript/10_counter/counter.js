29821fbef59871f1055a43e5df30dad911c696aa
shingayichitaka <shingayidambudzochitaka@gmail.com>
2022-08-19 01:39:04 +0200

    fix(pages/javascript): fix counter project update variable

--- pages/javascript/10_counter/counter.js
+++ pages/javascript/10_counter/counter.js
@@ -4,9 +4,13 @@ const display = document.querySelector(".display");
 const add = document.querySelector(".add");
 const lower = document.querySelector(".lower");
 
+let counter = 0;
+
 add.addEventListener("click", function () {
-  console.log("Click");
+  counter++;
+  display.textContent = counter;
 });
 lower.addEventListener("click", function () {
-  console.log("Click");
+  counter--;
+  display.textContent = counter;
 });
