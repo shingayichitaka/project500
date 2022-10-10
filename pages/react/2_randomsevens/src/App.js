2fe26a0f1c26839008b091a96f8ba1b76da73cdc
shingayichitaka <shingayidambudzochitaka@gmail.com>
2022-10-10 14:49:19 +0200

    feat(pages/react): add randomsevens project

---
+++ pages/react/2_randomsevens/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "./styles/styles.css";
+import Game from "./components/Game.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
